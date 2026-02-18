23d1af001d24310d1a45929b51af2d67d7a7435c
阿宅 <39202956+linshanghui@users.noreply.github.com>
2026-02-19 06:13:44 +0800

    Update Announcement.js

--- themes/gitbook/components/Announcement.js
+++ themes/gitbook/components/Announcement.js
@@ -4,18 +4,8 @@ import dynamic from 'next/dynamic'
 const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
 const Announcement = ({ notice, className }) => {
-//   const { locale } = useGlobal()
-  if (notice?.blockMap) {
-    return <div className={className}>
-        <section id='announcement-wrapper' className="dark:text-gray-300 rounded-xl px-2 py-4">
-            {/* <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div> */}
-            {notice && (<div id="announcement-content">
-            <NotionPage post={notice} />
-        </div>)}
-        </section>
-    </div>
-  } else {
-    return <></>
-  }
+  // 直接返回空，不渲染任何公告内容
+  return null
 }
+
 export default Announcement
